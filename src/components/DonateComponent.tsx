28dc3a0f2acbbbb5046f6fdb749254596946f378
Michael Sullihan <msullivan@APA-IT-LQNKC4.local>
2022-10-22 13:00:04 -0500

    Adding Routes

---
+++ src/components/DonateComponent.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { InteractiveLink } from '../ui/InteractiveLink';
+import { P } from '../ui/Paragraph';
+
+export const DonateComponent: React.VFC = () => (
+  <div>
+    <P>
+      Donate
+    </P>
+  </div>
+);
--- src/components/HistoryComponent.tsx
+++ src/components/HistoryComponent.tsx
@@ -3,12 +3,19 @@ import { InteractiveLink } from '../ui/InteractiveLink';
 import { P } from '../ui/Paragraph';
 
 export const HistoryComponent: React.VFC = () => (
-  <div>
-    <P>
-      This is an example page. Refresh the page or copy/paste the url to test
-      out the redirect functionality (this same page should load after the
-      redirect).
-    </P>
+  <div><P>
+    History:</P>
+<P>In the fall of 2012, a group of friends were finishing up a study group of ‘The New Jim Crow’ by Michelle Alexander, and were looking for a way to take action based on what they’d been learning about the prison industrial complex.  At the same time, Black & Pink National put out a call looking for hosts for the annual holiday card party.  Together, this multi-racial, mostly queer group of friends decided to host a holiday card party and organize a workshop with the Prison Industrial Complex Teaching Collective.  Over 40 people attended, 120+ cards were sent out, and the return address for the cards was listed as the housing co-op that had hosted the event.  Within a few weeks, over 60 inside members of Black & Pink in Illinois had written back, asking for continued correspondence and an array of requests for support & solidarity.  </P>
+
+<P>The small group of free-world friends began convening monthly meetings to read and respond to incoming mail, begin matching more pen-pals, and ultimately determine how best to respond to the clear desire from inside Black & Pink members for more queer & trans abolitionist support.  These meetings were attended by the early members of what would become the Black & Pink Chicago chapter, along with key people and groups involved in LGBTQ prison abolition work in Chicago and elsewhere. </P>
+
+<P>At the Chicago Dyke March in 2013, we formally launched our Chicago Chapter of Black & Pink, by marching together with a “Fuck Prisons” banner, distributing postcards recruiting new pen-pals, and inviting people to an upcoming film screening.  That summer, we began hosting weekly mail nights on Monday nights at the housing co-op to read and respond to incoming mail, and met twice per month as a chapter to determine more of our strategy and structure as free-world members of Black & Pink Chicago.  We created working groups in an attempt to hold down the various pieces of work that were being asked of us, including political education, mail processing, pen-pal matching, advocacy, and eventually re-entry.</P>
+
+<P>In the early months and even years, we struggled with how to develop systems and processes that would allow us to meet the overwhelming needs for support being asked of us, as an all volunteer group, with still relatively under-defined goals but giant hearts and visions.  We struggled with how to balance our desires to do outside organizing and political education with the very urgent and crisis-driven demands coming from inside members experiencing the violence and trauma of incarceration first-hand.  We struggled with race and class and power and how to share responsibilities and hold each other accountable, when we were taking on enormous workloads without necessarily providing training or onboarding for new people or ourselves.  But we continued to try to do the messy, needed work of building inside/outside relationships, and striving for the abolitionist commitments we’ve made to show up for each other and refuse the violent isolation that prisons impose on our communities. </P>
+<P>In 2022, Black & Pink Chicago decided unanimously to part from Black & Pink National. This decision was not made lightly, and was the result of many months of deliberation and feedback from inside members and formerly incarcerated members, and overall long-term independence and lack of support from the national chapter. </P>
+<P>We are indebted to the hundreds of volunteers who made the work of Black & Pink Chicago possible over the years and are honored to carry it forward into a new generation as Pushing Envelopes Chicago. We are armed with our lessons learned, our passion for transformation, and our ongoing love and solidarity with our family on the inside.</P>
+<P>Once there were no prisons.  That day will come again.</P>
+
     <InteractiveLink to="/example/two-deep?field1=foo&field2=bar#boom!">
       Example two deep with query and hash
     </InteractiveLink>
